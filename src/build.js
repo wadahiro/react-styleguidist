e225504cbfb9eb6ab21411915d7e8840c9812b0a
Artem Sapegin <artem@sapegin.ru>
2015-09-19 18:42:13 +0200

    Update 2015-09-19T16:42:13.529Z

--- src/build.js
+++
@@ -1,9 +0,0 @@
-var webpack = require('webpack');
-var makeWebpackConfig = require('./make-webpack-config');
-var config = require('./utils/config');
-
-console.log('Building style guide...');
-
-webpack(makeWebpackConfig('production'), function(err, stats) {
-	console.log('Done.');
-});
